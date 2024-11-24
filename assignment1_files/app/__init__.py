131b075b2d7efca26868ff10c6cc02de19fbd70b
javligten <99410951+javligten@users.noreply.github.com>
2024-11-24 22:13:17 +0100

    Updated code __init__.py

--- assignment1_files/app/__init__.py
+++ assignment1_files/app/__init__.py
@@ -1,1 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
 
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '../movies.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your-secret-key'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+login_manager.login_view = 'login'
+
+from app import routes
